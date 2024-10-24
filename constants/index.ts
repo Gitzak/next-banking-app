421336a1117aa91e984bb4481f387584ce18daa5
GITZAK <zakaria.belrhali@gmail.com>
2024-10-24 15:41:57 +0100

    Update index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -22,15 +22,14 @@ export const sidebarLinks = [
 ];
 
 // good_user / good_password - Bank of America
-export const TEST_USER_ID = "6627ed3d00267aa6fa3e";
+export const TEST_USER_ID = "";
 
 // custom_user -> Chase Bank
 // export const TEST_ACCESS_TOKEN =
 //   "access-sandbox-da44dac8-7d31-4f66-ab36-2238d63a3017";
 
 // custom_user -> Chase Bank
-export const TEST_ACCESS_TOKEN =
-  "access-sandbox-229476cf-25bc-46d2-9ed5-fba9df7a5d63";
+export const TEST_ACCESS_TOKEN =  "";
 
 export const ITEMS = [
   {
